36c28e8fe63d8d958a170eb3496730d3829fdb27
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2024-12-26 21:42:13 +0600

    Header design and general style added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import "./App.css";
-
+import Header from "./component/Header";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <main>
+      <Header />
       <div className="form">
         <label>
           Top Text
---
+++ src/component/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <img src="troll-face-header.png" alt="troll face icone" />
+      <h1>Meme Generator</h1>
+    </div>
+  );
+};
+
+export default Header;
